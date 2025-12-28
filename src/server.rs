7b3c1b7890887dc2e17a184cafd980f1b3417f0c
Christopher Skene <chris.skene@gmail.com>
2025-12-29 01:08:01 +1100

    feat: add hot-reload for jumble files

--- src/server.rs
+++ src/server.rs
@@ -16,18 +16,15 @@ use crate::tools::{self, ProjectData};
 pub struct Server {
     pub root: PathBuf,
     pub workspace: Option<WorkspaceConfig>,
-    pub projects: HashMap<String, ProjectData>,
 }
 
 impl Server {
     pub fn new(root: PathBuf) -> Result<Self> {
         let workspace = Self::load_workspace_static(&root);
-        let mut server = Server {
+        let server = Server {
             root,
             workspace,
-            projects: HashMap::new(),
         };
-        server.discover_projects()?;
         Ok(server)
     }
 
@@ -43,7 +40,8 @@ impl Server {
         None
     }
 
-    fn discover_projects(&mut self) -> Result<()> {
+    fn discover_projects(&self) -> Result<HashMap<String, ProjectData>> {
+        let mut projects = HashMap::new();
         for entry in WalkDir::new(&self.root)
             .follow_links(true)
             .into_iter()
@@ -63,14 +61,14 @@ impl Server {
                     let conventions = self.load_conventions(path.parent().unwrap());
                     let docs = self.load_docs(path.parent().unwrap());
 
-                    self.projects.insert(
+                    projects.insert(
                         config.project.name.clone(),
                         (project_dir, config, prompts, conventions, docs),
                     );
                 }
             }
         }
-        Ok(())
+        Ok(projects)
     }
 
     fn discover_prompts(&self, jumble_dir: &Path) -> ProjectPrompts {
@@ -177,18 +175,25 @@ impl Server {
 
         let arguments = params.get("arguments").cloned().unwrap_or(json!({}));
 
+        // Rebuild projects on each call for hot reloading
+        let projects = self.discover_projects().map_err(|e| JsonRpcError {
+            code: -32603,
+            message: format!("Failed to discover projects: {}", e),
+            data: None,
+        })?;
+
         let result = match name {
-            "list_projects" => tools::list_projects(&self.projects),
-            "get_project_info" => tools::get_project_info(&self.projects, &arguments),
-            "get_commands" => tools::get_commands(&self.projects, &arguments),
-            "get_architecture" => tools::get_architecture(&self.projects, &arguments),
-            "get_related_files" => tools::get_related_files(&self.projects, &arguments),
-            "list_prompts" => tools::list_prompts(&self.projects, &arguments),
-            "get_prompt" => tools::get_prompt(&self.projects, &arguments),
-            "get_conventions" => tools::get_conventions(&self.projects, &arguments),
-            "get_docs" => tools::get_docs(&self.projects, &arguments),
+            "list_projects" => tools::list_projects(&projects),
+            "get_project_info" => tools::get_project_info(&projects, &arguments),
+            "get_commands" => tools::get_commands(&projects, &arguments),
+            "get_architecture" => tools::get_architecture(&projects, &arguments),
+            "get_related_files" => tools::get_related_files(&projects, &arguments),
+            "list_prompts" => tools::list_prompts(&projects, &arguments),
+            "get_prompt" => tools::get_prompt(&projects, &arguments),
+            "get_conventions" => tools::get_conventions(&projects, &arguments),
+            "get_docs" => tools::get_docs(&projects, &arguments),
             "get_workspace_overview" => {
-                tools::get_workspace_overview(&self.root, &self.workspace, &self.projects)
+                tools::get_workspace_overview(&self.root, &self.workspace, &projects)
             }
             "get_workspace_conventions" => {
                 tools::get_workspace_conventions(&self.workspace, &arguments)
